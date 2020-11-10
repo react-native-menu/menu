440d513c62a344e7945adeaa2e388749fdae0845
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-11-10 15:24:52 +0900

    chore: add children for unimplemented view

--- src/UIMenuView.tsx
+++ src/UIMenuView.tsx
@@ -2,8 +2,11 @@ import * as React from 'react';
 import { View } from 'react-native';
 import type { MenuComponentProps } from './types';
 
-const MenuView: React.FC<MenuComponentProps> = () => {
-  return <View />;
+/**
+ * TODO: implement for Android and Web
+ */
+const MenuView: React.FC<MenuComponentProps> = ({ style, children }) => {
+  return <View style={style}>{children}</View>;
 };
 
 export default MenuView;

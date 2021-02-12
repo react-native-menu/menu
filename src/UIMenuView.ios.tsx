d2a9605d599bac710c0f4f2355734a66139976ab
Jesse Katsumata <jesse.katsumata@gmail.com>
2021-02-12 13:48:04 +0900

    chore: update prettier (#34)

--- src/UIMenuView.ios.tsx
+++ src/UIMenuView.ios.tsx
@@ -1,8 +1,8 @@
 import { HostComponent, requireNativeComponent } from 'react-native';
 import type { MenuComponentProps } from './types';
 
-const MenuComponent = requireNativeComponent('RCTUIMenu') as HostComponent<
-  MenuComponentProps
->;
+const MenuComponent = requireNativeComponent(
+  'RCTUIMenu'
+) as HostComponent<MenuComponentProps>;
 
 export default MenuComponent;

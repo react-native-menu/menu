99a04d6df4965e2ee6e9dfdaaedefdbb4284ec29
Joni Van Roost <joni.vr@hotmail.com>
2021-11-26 13:52:12 +0100

    fix(iOS): action sheet empty title being displayed when not defining title

--- ios/ActionSheetView.swift
+++ ios/ActionSheetView.swift
@@ -11,14 +11,9 @@ import UIKit
 @objc(ActionSheetView)
 class ActionSheetView: UIView {
     @objc var onPressAction: RCTDirectEventBlock?
-    private var _title: String = "";
+    private var _title: String?;
     @objc var title: NSString? {
-        didSet {
-            guard let title = self.title else {
-                return
-            }
-            self._title = title as String
-        }
+        didSet { self._title = title as? String }
     }
     
     private var _actions: [UIAlertAction] = [];

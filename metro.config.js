2f5bd6c324efe6426fbe80f6f002f19094aff7ee
Saad Najmi <sanajmi@microsoft.com>
2024-04-12 05:01:41 -0500

    Update metro config

--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,20 @@
 const path = require('path');
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 const { makeMetroConfig } = require('@rnx-kit/metro-config');
-module.exports = mergeConfig(
-  getDefaultConfig(__dirname),
-  makeMetroConfig({
-    projectRoot: path.join(__dirname, 'example'),
-    watchFolders: [__dirname],
-    resolver: {
-      extraNodeModules: {
-        '@react-native-menu/menu': __dirname,
+module.exports = makeMetroConfig({
+  projectRoot: path.join(__dirname, 'example'),
+  watchFolders: [__dirname],
+  resolver: {
+    extraNodeModules: {
+      '@react-native-menu/menu': __dirname,
+    },
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
       },
-    },
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: false,
-        },
-      }),
-    },
-  })
-);
+    }),
+  },
+});

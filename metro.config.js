013bb2255fc818ac7e625ca3215e34fd5fc0ee22
Saad Najmi <sanajmi@microsoft.com>
2024-04-13 20:57:25 -0500

    feat: Add visionOS support (#748)
    * Update to React Native 0.73
    
    * Remove extra package.json
    
    * Use yarn 4
    
    * yarn dedupe
    
    * Add visionOS support
    
    * Apply suggestions from code review
    
    Co-authored-by: Tommy Nguyen <4123478+tido64@users.noreply.github.com>
    
    * Update metro config
    
    * udpate lock
    
    ---------
    
    Co-authored-by: Tommy Nguyen <4123478+tido64@users.noreply.github.com>

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,14 @@
+const path = require('path');
+
 const { makeMetroConfig } = require('@rnx-kit/metro-config');
 module.exports = makeMetroConfig({
+  projectRoot: path.join(__dirname, 'example'),
+  watchFolders: [__dirname],
+  resolver: {
+    extraNodeModules: {
+      '@react-native-menu/menu': __dirname,
+    },
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {

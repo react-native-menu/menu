d4ef0eb3953723ea5eef959352bf1e85af9daa17
Grégoire Van der Auwermeulen <gregoirevandera@gmail.com>
2026-06-18 14:37:12 +0200

    feat(ios): action(s)

--- ios/NewArch/MenuView.mm
+++ ios/NewArch/MenuView.mm
@@ -134,7 +134,7 @@ using namespace facebook::react;
             @"image": [NSString stringWithUTF8String:action.image.c_str()],
             @"imageColor": @(action.imageColor),
             @"displayInline": @(action.displayInline),
-            @"preferredElementSize": @(actions.preferredElementSize),
+            @"preferredElementSize": @(action.preferredElementSize),
             @"attributes": @{
                 @"destructive": @(action.attributes.destructive),
                 @"disabled": @(action.attributes.disabled),
